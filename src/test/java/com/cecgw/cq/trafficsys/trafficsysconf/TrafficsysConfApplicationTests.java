f7b31a65e80844b58a7b33ea6e5029c2fd422468
yydeng <dengyy@cartechfin.com>
2019-05-16 08:39:49 +0800

    init

---
+++ src/test/java/com/cecgw/cq/trafficsys/trafficsysconf/TrafficsysConfApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cecgw.cq.trafficsys.trafficsysconf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrafficsysConfApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
